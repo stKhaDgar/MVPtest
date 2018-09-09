adbb70e3016fe89c6471327e26a886266ff54aa4
Andrey Berezhnoi <stmaktavish@gmail.com>
2018-09-09 19:24:52 +0300

    clear

--- app/src/main/java/com/example/stmak/mvptest/presenter/MainActivityPresenter.kt
+++ app/src/main/java/com/example/stmak/mvptest/presenter/MainActivityPresenter.kt
@@ -2,7 +2,6 @@ package com.example.stmak.mvptest.presenter
 import com.example.stmak.mvptest.MainActivityContract
 
 class MainActivityPresenter(var view: MainActivityContract.IView) : MainActivityContract.IPresenter {
-
     override fun clickedToastButton() {
         view.displayToastMessage()
     }
